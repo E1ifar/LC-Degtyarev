6cc4aa3c027fc840575fdbf6aa0743f37109a1cc
Anton <degtyarevan2004@yandex.ru>
2022-09-20 22:49:50 +0300

    new

--- lr2/pr11.py
+++ lr2/pr11.py
@@ -2,4 +2,5 @@ import math
 x=6.251;
 y=0.827;
 z=25.001;
-s=y**((math.fabs(x))**(1/2))+(math.cos(y)**3)*((math.fabs(x-y)*(1+)))
+s=y**((math.fabs(x))**(1/3))+(math.cos(y)**3)*((math.fabs(x-y)*(1+((math.sin(z)**2)/(math.sqrt(x+y))))))/(math.e**(math.fabs(x-y))+x/2);
+print(s);
