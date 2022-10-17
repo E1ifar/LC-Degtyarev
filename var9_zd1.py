fdaf1f736e6bdec37629b5c28f3db25f50a1e1aa
Anton <degtyarevan2004@yandex.ru>
2022-10-17 13:22:57 +0300

    Create var9_zd1.py

---
+++ var9_zd1.py
@@ -0,0 +1,17 @@
+n=[];
+def cifr(a):
+    while a>1:
+        n.append(a%10);
+        a=a//10;
+    return n;
+a=int(input("Введите число: "));
+i=0;
+d=a;
+while d>0:
+    print(cifr(d));
+    s=sum(n);
+    i=i+1
+    d=d-s;
+    n.clear();
+    print(d);
+print(i);
