dac0d81e0367b2ffe533ef1f8cc8cb3b3f01b69d
Anton <degtyarevan2004@yandex.ru>
2022-11-07 19:52:08 +0300

    .

--- lr9/задание1/zadanie1.py
+++ lr9/задание1/zadanie1.py
@@ -1,21 +1,20 @@
 #Ввод и вывод данных матриц из практической номер 8
 
-n=int(input("Введите значение квадратной матрицы: "))
- #создание массива
- 
+n=int(input("Введите значение квадратной матрицы: ")) 
+with open('lr9\задание1\Degtyarev_vvod1.txt', 'r') as file: #получение данных из файла
+    mas1 = []
+    for mas1_line in file.readlines():
+        line = mas1_line.split()
+        mas1 += line
+    mas1 = list(map(int, mas1))
 
-#with open('lr9\задание1\Degtyarev_vvod1.txt','r') as file:
-    #for i in range(len(mas)):
-     #   for j in range (len(mas[i])):
-      #      for line in file:
-       #         mas.append([int(x) for x in line.split()]) 
+mas = []
+for l in range(n):
+    line = [mas1.pop(0) for l in range(n)]
+    mas.append(line) # преобразование данных полученных выше в массив
+    
+print(mas)
 
-with open('lr9\задание1\Degtyarev_vvod1.txt', 'r') as file:
-    mass = [line.split() for line in file]
-print(mass)
-
-
-mas=[]    
 print("Массив: ")
 for i in range(n): #Вывод массива
     for j in range(n):
@@ -36,9 +35,3 @@ for i in range(len(mas)): #нахождение чисел кратных k
             if mas[i][j]%k==0:
                 l+=1
 print("Кол-во чисел кратных k: %s" %l)
-
-print("Массив: ")
-for i in range(n): #Вывод массива
-    for j in range(n):
-        print(mas[i][j], end = ' ')
-    print()
